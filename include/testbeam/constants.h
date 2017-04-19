ce05c1c38a3d9179245a9fc0e1f6858a34073755
Alon Hershenhorn <hershen@phas.ubc.ca>
2017-04-19 13:19:53 -0700

    added TOF means and sigmas for predicted time at crystal to constants.h?

--- include/testbeam/constants.h
+++ include/testbeam/constants.h
@@ -12,5 +12,18 @@ constexpr double c_downstreamCenter2incubatorWall = 0.0419; // meters
 
 constexpr double c_crystalLength = 0.3; // meters
 
+
+//maps of [beam momentum] = value.
+//For mean and sigma.
+//Values calculated from fitting a gaussian to around mean +- 2 sigma in beta distribtuion.
+//Done in script findTOFcrystalTimesAndSigmas.C
+const std::unordered_map<int, double> muonBetaMean = { {-100, 0.72961},{-120, 0.786136}, {-140, 0.828577} }; 
+const std::unordered_map<int, double> pionBetaMean = { {-100, 0.605955},{-120, 0.680284}, {-140, 0.741776} }; 
+
+const std::unordered_map<int, double> electronBetaSigma = { {-100, 0.028034},{-120, 0.02957}, {-140, 0.028172} }; 
+const std::unordered_map<int, double> muonBetaSigma = { {-100, 0.013659},{-120, 0.015451}, {-140, 0.017359} }; 
+const std::unordered_map<int, double> pionBetaSigma = { {-100, 0.010849},{-120, 0.011701}, {-140, 0.013979} }; 
+
+
 }
 }
