35bd7a1edfd37ff0d9bcde37c84dae646955a113
Alon Hershenhorn <hershen@phas.ubc.ca>
2018-01-04 10:02:20 -0800

    Added channel 1 channel 15 time difference constant

--- include/testbeam/constants.h
+++ include/testbeam/constants.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <unordered_map>
 #include <map>
+#include <unordered_map>
 
 namespace myFuncs {
 namespace testbeam {
@@ -26,9 +26,14 @@ constexpr double c_upstreamLeadCollimatorFacesDistance = 0.02; // meters. Based 
 
 constexpr double c_crystalLength = 0.3; // meters
 
+// I.e., to align channel1 and channel 15 in time, channel 1 needs to be moved 1.088 ns to the right.
+// Based on Timing resolution CR-RC4 + HPF
+constexpr double c_channel1_channel15_timeDifference = 1.088; // ns.
+
 using CrystalAdc2meV = std::unordered_map<int, double>;
 const static CrystalAdc2meV c_CsI_Tl_Belle_Adc2MeV{{1, 0.7073}, {15, 0.7209}}; // [ADC counts / MeV]
-const static std::map<Crystal, CrystalAdc2meV> c_crystal2_adc2mev{{Crystal::CsI_Tl_Belle, c_CsI_Tl_Belle_Adc2MeV}}; //Unordered map doesn't currently work with enums. Maybe with c++14
+const static std::map<Crystal, CrystalAdc2meV> c_crystal2_adc2mev{
+    {Crystal::CsI_Tl_Belle, c_CsI_Tl_Belle_Adc2MeV}}; // Unordered map doesn't currently work with enums. Maybe with c++14
 
 // maps of [beam momentum] = value.
 // For mean and sigma.
