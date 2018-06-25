a90629cbc6e2bffed0eb6fea9409def38ac37a68
Alon Hershenhorn <hershen@gmail.com>
2018-06-25 13:43:42 -0700

    hide auto_ptr warning. 2) remove commented include

--- pythonBindings/src/myRootStyle.cxx
+++ pythonBindings/src/myRootStyle.cxx
@@ -1,8 +1,11 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
 #include <boost/python.hpp>
+#pragma GCC diagnostic pop
 
-// #include "myRootStyle.h"
 #include "src/myRootStyle.cxx"
 using namespace boost::python;
+
 
 BOOST_PYTHON_MODULE(myRootStyle)
 {
