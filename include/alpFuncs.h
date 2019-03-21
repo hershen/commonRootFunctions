8be2af60afd55a8d48ed86457429a8b15c780b7e
Alon Hershenhorn <hershen@gmail.com>
2019-03-21 14:55:41 -0700

    Comment getAlpMassDouble because it probably has a bug in the equation of the face of the endcap. Added getDistanceToEmcFace_m

--- include/alpFuncs.h
+++ include/alpFuncs.h
@@ -8,6 +8,6 @@ double getAlpMassDouble(const std::string& text);
 
 //x,y,z in cm
 //based on https://www.slac.stanford.edu/BFROOT/www/Detector/Calorimeter/software/geometry.html
-bool inEMC(const double x, const double y, const double z);
+// bool inEMC(const double x, const double y, const double z); line equation for face of forward endcap is probably wrong, see getDistanceToEmcFace_m
 
 } // namespace myFuncs
--- src/alpFuncs.cxx
+++ src/alpFuncs.cxx
@@ -1,3 +1,5 @@
+#include <limits>
+
 #include <boost/lexical_cast.hpp>
 
 #include "TVector3.h"
@@ -21,7 +23,25 @@ bool inEMC(const double x, const double y, const double z) {
   }
 
   // line equation for face of forward endcap (in z-r plane) r=-2.39z + 522.2
+  //Might really be r = -2.659z + 5.707 (all in meters)
   const double rEndcapFace = -2.39 * vtx.z() + 522.2;
   return r > rEndcapFace;
 }
+
+double getDistanceToEmcFace_m(const double theta_rad) {
+  if(theta_rad < 0.27576202181510407315 or theta_rad > 2.47487687932795934008) { //[15.8, 141.8]
+    return std::numeric_limits<double>::infinity();
+  }
+  if(theta_rad < 0.47249693551706384232) { //endcap
+    //Front face of endcap in z-r plane: r = -2.659z+5.707 (all in meters).
+    const double z = 5.707/(std::tan(theta_rad)+2.659);
+    return z/std::cos(theta_rad);
+  }
+  if(theta_rad < 2.475) {
+    return 0.92/std::sin(theta_rad);
+  }
+  
+  throw; 
+}
+
 } // namespace myFuncs
